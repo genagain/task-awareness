8fcaa5473f43a3844b40d843eb51cfda428b6e25
Gen Ohta <genohta@Gens-MacBook-Pro.local>
2019-02-01 21:24:04 -0500

    extract from DB with a value

--- taskawareness/trello.py
+++ taskawareness/trello.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 import json
 
+import psycopg2
+import psycopg2.extras
 import requests
 
 def fetch_actions():
@@ -28,8 +30,29 @@ def filter_date(action, date):
     datetime_est = parse_date(action['date'], convert_est=True)
     return date == datetime_est.strftime('%Y-%m-%d')
 
-def get_archived_cards(date):
-    actions = fetch_actions()
+def execute_sql(query):
+    # TODO make this a test DB
+    conn = psycopg2.connect("dbname=taskawareness_dev user=postgres")
+
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute(query)
+
+    # TODO consider breaking up connection, execution and reading records
+
+    all_records = []
+    for raw_record in cur.fetchall():
+        record = [(key,value.strftime('%Y-%m-%d %H:%M:%S'))
+                  if type(value).__name__ == 'datetime'
+                  else
+                  (key,value)
+                  for key, value
+                  in raw_record.items()]
+
+        all_records.append(dict(record))
+
+    return all_records
+
+def store_archived_cards(actions, date):
     date_actions = [action for action in actions if filter_date(action, date)]
     archived_actions = [action for action in date_actions if archived(action)]
     archived_cards = []
@@ -45,4 +68,31 @@ def get_archived_cards(date):
             'card_name': card_name
         }
         archived_cards.append(archived_card)
+    # TODO bulk insert here
+
+    return True
+
+def store_completed_items(actions, date):
+    return True
+
+# TODO make this a DB read operation
+def get_archived_cards(date):
+    # TODO parsing archived cards a DB write operation
+    actions = fetch_actions()
+    date_actions = [action for action in actions if filter_date(action, date)]
+    archived_actions = [action for action in date_actions if archived(action)]
+    archived_cards = []
+    for action in archived_actions:
+        datetime = parse_date(action['date']).strftime('%Y-%m-%d %H:%M:%S')
+        board_id = action['data']['board']['id']
+        card_id = action['data']['card']['id']
+        card_name = action['data']['card']['name']
+        archived_card = {
+            'datetime': datetime,
+            'board_id': board_id,
+            'card_id': card_id,
+            'card_name': card_name
+        }
+        archived_cards.append(archived_card)
+    # TODO bulk insert here
     return archived_cards
--- tests/test_trello.py
+++ tests/test_trello.py
@@ -4,10 +4,14 @@ from freezegun import freeze_time
 
 from taskawareness import trello
 
+
+# TODO make a fixture that sets up and tears down tables
+
 @freeze_time("Jan 29, 2019")
-def test_get_archived_cards(monkeypatch):
+def test_store_archived_cards(monkeypatch):
     def mock_fetch_actions():
         # TODO make this relative project root
+        # TODO use this mock for testing DB storage functions
         with open('archived_card_action.json', 'r') as f:
             data = json.loads(f.read())
 
@@ -15,14 +19,21 @@ def test_get_archived_cards(monkeypatch):
 
     monkeypatch.setattr(trello, 'fetch_actions', mock_fetch_actions)
 
-    archived_cards = [
+    actions = trello.fetch_actions()
+
+    # TODO make sure this passes with an empty table
+    trello.store_archived_cards(actions, '2019-01-29')
+
+    expected_archived_cards = [
         {
-            'datetime': '2019-01-30T00:50:47',
+            'datetime': '2019-01-29 19:50:47',
             'board_id': '5c4ef5558ac287209796dace',
             'card_id': '5c50f4ddb89030449f74f47b',
             'card_name': 'Testing'
         }
     ]
-    assert trello.get_archived_cards('2019-01-29') == archived_cards
 
+    query = 'SELECT datetime, board_id, card_id, card_name FROM cards;'
+    actual_archived_cards = trello.execute_sql(query)
 
+    assert actual_archived_cards == expected_archived_cards
