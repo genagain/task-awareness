7aee9e15e4c6afae3f6a1100fe85be57d1ccfbdb
Gen Ohta <genohta@Gens-MacBook-Pro.local>
2019-02-10 20:11:39 -0500

    temporarily comment out scheduled job decorator to verify deployment

--- clock.py
+++ clock.py
@@ -6,10 +6,11 @@ from taskawareness import trello
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=22, minute=30)
+# @sched.scheduled_job('cron', hour=22, minute=30)
 def scheduled_job():
     now = datetime.now()
     today = now.strftime('%Y-%m-%d')
     actions = trello.fetch_actions()
     trello.store_archived_cards(actions, today)
 
+scheduled_job()
