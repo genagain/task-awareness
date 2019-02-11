9b1a4d0808ffe923970c67521c2e0ef3c54c3301
Gen Ohta <genohta@Gens-MacBook-Pro.local>
2019-02-10 20:46:10 -0500

    enable scheduler change time to be 9:30 pm EST

--- clock.py
+++ clock.py
@@ -6,11 +6,9 @@ from taskawareness import trello
 
 sched = BlockingScheduler()
 
-# @sched.scheduled_job('cron', hour=22, minute=30)
+@sched.scheduled_job('cron', hour=2, minute=30)
 def scheduled_job():
     now = datetime.now() - timedelta(hours=5)
     today = now.strftime('%Y-%m-%d')
     actions = trello.fetch_actions()
     trello.store_archived_cards(actions, today)
-
-scheduled_job()
