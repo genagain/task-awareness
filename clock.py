ed273e4f6bd74a778bb87cf6f956d0452a5f9127
Gen Ohta <genohta@Gens-MacBook-Pro.local>
2019-02-10 23:32:09 -0500

    ensure utc scheduler

--- clock.py
+++ clock.py
@@ -4,7 +4,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 from taskawareness import trello
 
-sched = BlockingScheduler()
+sched = BlockingScheduler(timezone='utc')
 
 def scheduled_job():
     now = datetime.now() - timedelta(hours=5)
@@ -12,6 +12,6 @@ def scheduled_job():
     actions = trello.fetch_actions()
     trello.store_archived_cards(actions, today)
 
-sched.add_job(scheduled_job, 'cron', day_of_week='mon-sun', hour=4, minute=30)
+sched.add_job(scheduled_job, 'cron', day_of_week='mon-sun', hour=4, minute=40)
 
 sched.start()
