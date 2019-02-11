6ef35b6f4ad73a3cdc5f4b1064a202cd06cc347f
Gen Ohta <genohta@Gens-MacBook-Pro.local>
2019-02-10 22:36:35 -0500

    reschedule to 11 pm EST and actually start scheduler

--- clock.py
+++ clock.py
@@ -6,9 +6,11 @@ from taskawareness import trello
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=3, minute=30)
+@sched.scheduled_job('cron', hour=4)
 def scheduled_job():
     now = datetime.now() - timedelta(hours=5)
     today = now.strftime('%Y-%m-%d')
     actions = trello.fetch_actions()
     trello.store_archived_cards(actions, today)
+
+sched.start()
